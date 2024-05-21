3153d3e78fee008967b4677fa0c85ccd415f2478
unknown <paccyhabi@gmail.com>
2024-05-21 12:31:06 +0200

    testing commit

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,6 +2,7 @@ module.exports = {
     HOST: 'dpg-cp65s4ol6cac738eg0mg-a',
     USER: 'attendance_management_wjgq_user',
     PASSWORD: 'vph13QFY7MCyIa53OqPyANZUV2Fecr4N',
-    DB:'attendance_management_wjgq',
-    dialect: 'postgres'
-}
+    DB: 'attendance_management_wjgq',
+    dialect: 'postgres' 
+  }
+  
